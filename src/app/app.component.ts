3992ecf354e6efaf826579ee03b9fe3ac7c75f9b
Jigen21 <axel.quinteros@davinci.edu.ar>
2018-10-26 18:50:15 -0300

    dia 354:seguimos codeando el restaurante

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { TomarPedidoPage } from '../pages/tomar-pedido/tomar-pedido';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = EncuestaDeEmpleadoPage;
+  rootPage:any = TomarPedidoPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,modalCtrl: ModalController) {
     platform.ready().then(() => {
